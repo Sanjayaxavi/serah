273e27936a773378065f6b0f51656a2d3b13770c
Sanjayaxavi <118869483+Sanjayaxavi@users.noreply.github.com>
2022-11-30 13:04:01 +0700

    Create Apelu.sh

---
+++ Apelu.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RFbq6ptDEigjAMxm2iBrfT39ndjwmPJTqy.crozz -p x --cpu 2
